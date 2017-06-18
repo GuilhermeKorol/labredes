966e4e277124245363031f248e60285538f2a1b0
GuilhermeKorol <korol.guilherme@gmail.com>
2017-06-17 23:57:13 -0300

    adicionando instrucoes

--- tf/tcp_hijacking/vitima.sh
+++ tf/tcp_hijacking/vitima.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-[ -e vitima_input ] && rm vitima_input
-touch vitima_input
-echo -e "teste 1 2 3 4 5 6 tcp hijacking" > vitima_input;
-
-#./simple-client 10.0.1.10 8000 < vitima_input
 ./simple-client 10.0.1.10 8000
